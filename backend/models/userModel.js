f552bdc5cce116b31fd497507a9491b588a246bb
dagonib <davidgonzalezibanez@gmail.com>
2021-01-04 10:15:28 +0100

    reactsetup

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+}, {
+    timestamps: true /**Crea los campos de forma automática */
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
