38bd6ef199c31650780b7e10e15d77b1ffc557b5
dagonib <davidgonzalezibanez@gmail.com>
2021-01-05 18:30:06 +0100

    Use Authentication Endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,34 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-}, {
-    timestamps: true /**Crea los campos de forma automática */
-})
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+    }, {
+        timestamps: true /**Crea los campos de forma automática */
+    }
+)
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
