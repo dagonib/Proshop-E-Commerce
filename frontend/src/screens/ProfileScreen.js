8f83398603d71af1fbb44c48fed4e031f1a022af
dagonib <davidgonzalezibanez@gmail.com>
2021-01-14 18:36:57 +0100

    User details adn orders reset

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,7 +6,6 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { listMyOrders } from '../actions/orderActions'
-import { orderCreateReducer } from '../reducers/orderReducers'
 
 const ProfileScreen = ({ location, history }) => {
     const [name, setName] = useState('')
