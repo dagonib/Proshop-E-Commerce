4cb1f6a1fa00f633b1e734381db5e50ca2f7d025
dagonib <davidgonzalezibanez@gmail.com>
2021-02-02 18:46:48 +0100

    Preparing To Deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.json()) /**Permite aceptar data json en el body */
 
 
-app.get('/', (req, res)  => {
-    res.send('API is running...')
-})
+
 
 // End points
 app.use('/api/products', productRoutes)
@@ -40,6 +38,21 @@ app.get('/api/config/paypal', (req, res) =>
 //La carpeta ha de hacerse estatica
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+// Modo Producción
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res)  => {
+        res.send('API is running...')
+    })
+}
+
 
 app.use(notFound)
 
